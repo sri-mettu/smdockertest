1eea13f152b7672a82642ab3c352c806fb70cca4
sri <mettu.sri1@gmail.com>
2024-06-26 16:41:05 +0100

    updated test

--- src/test/java/sm/tests/test.java
+++ src/test/java/sm/tests/test.java
@@ -3,6 +3,9 @@ package sm.tests;
 import java.io.File;
 import java.io.IOException;
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
@@ -10,13 +13,15 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test; 
 
 public class test {
-@Test
+@Test (enabled=false)
 	public void Hwkeye () throws InterruptedException, IOException{
 	WebDriver driver = new ChromeDriver();
 	driver.manage().window().maximize();
@@ -41,4 +46,40 @@ public class test {
 	Thread.sleep(4000);
 	driver.quit();
 	}
+
+@Test
+public void HwkeyeJobsearch () throws InterruptedException, IOException{
+WebDriver driver = new ChromeDriver();
+driver.manage().window().maximize();
+driver.get("https://www.hawkeyeinnovations.com/");
+driver.findElement(By.id("comp-ku2rmt7t3label")).click();
+Set<String> allWindows = driver.getWindowHandles();
+List<String> windowList = new ArrayList<>(allWindows);
+// Switch to the new tab (second in the list)
+driver.switchTo().window(windowList.get(1));
+driver.findElement(By.id("onetrust-accept-btn-handler")).click();
+JavascriptExecutor js = (JavascriptExecutor) driver;
+js.executeScript("window.scrollBy(0,6000);");
+WebElement department = driver.findElement(By.cssSelector("select[name='departmentId']"));
+WebElement location = driver.findElement(By.cssSelector("select[name='locationId']"));
+WebElement employment = driver.findElement(By.cssSelector("select[name='employmentType']"));
+Select dept = new Select(department);
+dept.selectByValue("bdf1cf3b-e210-42f2-92cb-1668fe51cc1c");
+Select loc = new Select(location);
+loc.selectByValue("2768c69c-133a-4a84-8be5-41673c8a3f61");
+Select emptyp = new Select(employment);
+emptyp.selectByValue("FullTime");
+driver.findElement(By.xpath("h3[text()='QA Automation Engineer (Frontend)']")).click();
+
+Thread.sleep(4000);
+TakesScreenshot ts = (TakesScreenshot) driver;
+File source = ts.getScreenshotAs(OutputType.FILE);
+File file = new File(System.getProperty("user.dir") +"//reports//"+"screenshot" + ".png");
+FileUtils.copyFile(source, file);
+WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(60));
+wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//span[text()='Submit']"))); 
+//driver.findElement(By.xpath("//span[text()='Submit']")).click();
+Thread.sleep(4000);
+driver.quit();
+}
 }
